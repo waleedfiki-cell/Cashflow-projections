c08a481674c29428cd560e1b34fabf0ca6fb72a4
waleedfiki-cell <waleed.fiki@gmail.com>
2026-07-23 22:47:29 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,27 @@
-self.addEventListener('install', (event) => {
-      self.skipWaiting();
-      });
+const CACHE = 'ng-cashflow-v3';
+const FILES = ['./index.html', './manifest.json', './icon.svg'];
 
-      self.addEventListener('activate', (event) => {
-        event.waitUntil(self.clients.claim());
-        });
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(FILES))
+      .catch(() => {})
+  );
+  self.skipWaiting();
+});
 
-        self.addEventListener('fetch', (event) => {
-          event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-          });
-          
-})
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
